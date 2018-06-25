263efbef517f64066c5adb4bcaa2878019ceb37a
shaygyawali <shaygyawali@gmail.com>
2018-06-25 14:50:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,9 @@
 # dog.rb
+class Dog 
+  def initialize(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
+  end 
+end
+  
+fido = Dog.new("Fido","Pug")
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # person.rb
+class Person 
+  def initialize(person_name)
+    @name = person_name
+  end
+end
+
+beyonce = Person.new("Beyonce")
+
+    
+    
+    
